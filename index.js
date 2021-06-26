67a5ca60ae44a26288602913efbfd147c0fc0ff1
Paranidharan Thangavelu <tparanidharan@gmail.com>
2021-06-26 23:36:29 +0530

    added build

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':date[web] :method :url :status :response-time ms - :res[content-length] :sample'));
 app.use(cors())
