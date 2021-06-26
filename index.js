411349f4b609a1a07cd5938504ae4e11eab1c10e
Paranidharan Thangavelu <tparanidharan@gmail.com>
2021-06-27 00:59:51 +0530

    added fs formorgan loggin into file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+var fs = require('fs')
 const cors = require('cors')
 app.use(express.static('build'))
 app.use(express.json())
@@ -10,7 +11,10 @@ morgan.token('sample',function ( req) {
   //return JSON.stringify(req.body)
   return `{name:${req.body.name},number:${req.body.number}}`
 })
-
+app.use(morgan('common', {
+    stream: fs.createWriteStream('./logs/access.log', {flags: 'a'})
+}));
+app.use(logger('dev'));
 
 let persons = [
   {
