7528bcb08f2b2e7d5f739df209e988101a06eb9b
Paranidharan Thangavelu <tparanidharan@gmail.com>
2021-06-26 20:05:30 +0530

    Added server initial setup

--- index.js
+++ index.js
@@ -1,85 +1,116 @@
 const express = require('express')
 const app = express()
-
+const morgan = require('morgan')
+const cors = require('cors')
 app.use(express.json())
+app.use(morgan(':date[web] :method :url :status :response-time ms - :res[content-length] :sample'));
+app.use(cors())
+morgan.token('sample',function ( req) {
+  //return JSON.stringify(req.body)
+  return `{name:${req.body.name},number:${req.body.number}}`
+})
 
-
-let notes = [
+let persons = [
   {
     id: 1,
-    content: "HTML is easy",
-    date: "2020-01-10T17:30:31.098Z",
-    important: true
+    name: "Parani",
+    number: "2020-01-10",
   },
   {
     id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2020-01-10T18:39:34.091Z",
-    important: false
+    name: "Karthik",
+    number: "202010",
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2020-01-10T19:20:14.298Z",
-    important: true
-  }
+    name: "Kavitha",
+    number: "2020-10",
+  },
+  {
+    id: 4,
+    name: "Mithunya",
+    number: "20201-10",
+  },
 ]
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/persons', (req, res) => {
+  console.table(persons);
+  res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+res.send(`Phonebook has info of ${persons.length} contact${persons.length !==1 ? 's':''}
+  <br><br>${new Date()}`)
 })
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id)) + 1
-    :  1
+  const maxId = Math.floor(Math.random() * 1000)
   return maxId
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/persons', (request, response) => {
   const body = request.body
-
-  if (!body.content) {
+//  console.log(body, body.content);
+  if (!body.name && !body.number) {
     return response.status(400).json({
       error: 'content missing'
     })
   }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
+  else if (!body.name ) {
+    return response.status(400).json({
+      error: 'Name missing'
+    })
+  }
+  else if (!body.number ) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
     id: generateId(),
   }
+  if(!persons.find(person => person.name===body.name))
+{  persons = persons.concat(person)
+  response.json(person)
+}
+else {
+  response.status(400).json({error: "Person is already in contacts"})
+}
 
-  notes = notes.concat(note)
-
-  response.json(note)
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
+  const person = persons.find(person => person.id === id)
 
-  if (note) {
-    response.json(note)
+  if (person) {
+    response.json(person)
   } else {
     response.status(404).end()
   }
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
+  persons = persons.filter(person => person.id !== id)
   response.status(204).end()
 })
 
-const PORT = 3001
+app.put('/api/persons/:id', (request, response) => {
+  console.log(JSON.stringify(request.body))
+  const id = Number(request.params.id)
+  const Restpersons = persons.filter(person => person.id !== id)
+  persons=Restpersons.concat(request.body)
+  response.json(request.body)
+})
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
